b222360ea16ec35f924cd306095929793242e80b
Gauri Kasar <180354256+gkasar@users.noreply.github.com>
2024-10-22 22:56:17 -0700

    Removed hardcoded values for different attributes in main.tf modules

--- Zonal Resilience/Without Read Replica/main.tf
+++ Zonal Resilience/Without Read Replica/main.tf
@@ -2,23 +2,18 @@ provider "azurerm" {
   features {}
 }
 resource "azurerm_resource_group" "default" {
-  name     = "gkasar-terraform"
-  location = "Canada Central"
+  name     = <resource-name>
+  location = <region>
 }
 resource "azurerm_virtual_network" "example" {
-  name                = "example-vn"
+  name                = <vnet-name>
   location            = azurerm_resource_group.default.location
   resource_group_name = azurerm_resource_group.default.name
   address_space       = ["10.0.0.0/16"]
 }
-resource "azurerm_virtual_network" "default" {
-  name                = "vn-west"
-  location            = "Canada East"
-  resource_group_name = azurerm_resource_group.default.name
-  address_space       = ["10.0.0.0/16"]
-}
+
 resource "azurerm_network_security_group" "default" {
-  name                = "example-security-group"
+  name                = <nsg-name>
   location            = azurerm_resource_group.default.location
   resource_group_name = azurerm_resource_group.default.name
 
@@ -34,25 +29,9 @@ resource "azurerm_network_security_group" "default" {
     destination_address_prefix = "*"
   }
 }
-resource "azurerm_network_security_group" "example" {
-  name                = "west-security-group"
-  location            = "Canada East"
-  resource_group_name = azurerm_resource_group.default.name
 
-  security_rule {
-    name                       = "test123"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
 resource "azurerm_subnet" "example" {
-  name                 = "example-sn"
+  name                 = <subnet-name>
   resource_group_name  = azurerm_resource_group.default.name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.2.0/24"]
@@ -67,57 +46,32 @@ resource "azurerm_subnet" "example" {
     }
   }
 }
-resource "azurerm_subnet" "default" {
-  name                 = "crossregion-west"
-  resource_group_name  = azurerm_resource_group.default.name
-  virtual_network_name = azurerm_virtual_network.default.name
-  address_prefixes     = ["10.0.2.0/24"]
-  service_endpoints    = ["Microsoft.Storage"]
-  delegation {
-    name = "fs"
-    service_delegation {
-      name = "Microsoft.DBforPostgreSQL/flexibleServers"
-      actions = [
-        "Microsoft.Network/virtualNetworks/subnets/join/action",
-      ]
-    }
-  }
-}
-resource "azurerm_subnet_network_security_group_association" "default" {
-  subnet_id                 = azurerm_subnet.example.id
-  network_security_group_id = azurerm_network_security_group.default.id
-}
+
+
 resource "azurerm_subnet_network_security_group_association" "example" {
   subnet_id                 = azurerm_subnet.default.id
   network_security_group_id = azurerm_network_security_group.example.id
 }
 resource "azurerm_private_dns_zone" "example" {
-  name                = "gkasar.postgres.database.azure.com"
+  name                = "<name>.postgres.database.azure.com"
   resource_group_name = azurerm_resource_group.default.name
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "example" {
-  name                  = "exampleVnetZone.com"
+  name                  = "<vnet-name>.com"
   private_dns_zone_name = azurerm_private_dns_zone.example.name
   virtual_network_id    = azurerm_virtual_network.example.id
   resource_group_name   = azurerm_resource_group.default.name
   depends_on            = [azurerm_subnet.example]
 }
-resource "azurerm_private_dns_zone_virtual_network_link" "default" {
-  name                  = "westVnetZone.com"
-  private_dns_zone_name = azurerm_private_dns_zone.example.name
-  virtual_network_id    = azurerm_virtual_network.default.id
-  resource_group_name   = azurerm_resource_group.default.name
-  depends_on            = [azurerm_subnet.default]
-}
 resource "azurerm_postgresql_flexible_server" "default" {
-  name                          = "flex-instance-server"
+  name                          = <server-name>
   resource_group_name           = azurerm_resource_group.default.name
   location                      = azurerm_resource_group.default.location
   version                       = "16"
   public_network_access_enabled = false
-  administrator_login           = "psqladmin"
-  administrator_password        = "H@Sh1CoR3!"
+  administrator_login           = <user-name>
+  administrator_password        = <password>
   zone                          = ""
   high_availability {
     mode                      = "ZoneRedundant"
