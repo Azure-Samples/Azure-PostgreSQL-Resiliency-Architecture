a07e99163b6ea2bce183310f9484e48af84e0277
Gauri Kasar <180354256+gkasar@users.noreply.github.com>
2024-10-22 21:52:45 -0700

    Removed hard-coded values for different attributes in crossregionreplica.tf

--- Cross region replication/crossregionreplica.tf
+++ Cross region replication/crossregionreplica.tf
@@ -3,12 +3,12 @@ provider "azurerm" {
   subscription_id = "<subscriptionid>"
 }
 resource "azurerm_network_security_group" "example" {
-  name                = "west-security-group"
-  location            = "Central US"
-  resource_group_name = "gkasar-readreplica-terraform"
+  name                = <nsg-name>
+  location            = <region>
+  resource_group_name = <resource-name>
 
   security_rule {
-    name                       = "test123"
+    name                       = <name>
     priority                   = 100
     direction                  = "Inbound"
     access                     = "Allow"
@@ -20,20 +20,20 @@ resource "azurerm_network_security_group" "example" {
   }
 }
 resource "azurerm_virtual_network" "default" {
-  name                = "vn-west"
-  location            = "Central US"
-  resource_group_name = "gkasar-readreplica-terraform"
+  name                = <vnet-name>
+  location            = <region>
+  resource_group_name = <resource-name>
   address_space       = ["10.0.0.0/16"]
 }
 resource "azurerm_private_dns_zone" "default" {
   name                = "crossregion.postgres.database.azure.com"
-  resource_group_name = "gkasar-readreplica-terraform"
+  resource_group_name = <resource-name>
 }
 
 
 resource "azurerm_subnet" "default" {
-  name                 = "crossregion-west"
-  resource_group_name  = "gkasar-readreplica-terraform"
+  name                 = <subnet-name>
+  resource_group_name  = <resource-name>
   virtual_network_name = azurerm_virtual_network.default.name
   address_prefixes     = ["10.0.2.0/24"]
   service_endpoints    = ["Microsoft.Storage"]
@@ -49,11 +49,11 @@ resource "azurerm_subnet" "default" {
   depends_on            = [azurerm_virtual_network.default]
 }
 resource "azurerm_postgresql_flexible_server" "crossregion1" {
-  name                          = "firstreplica"
-  resource_group_name           ="gkasar-readreplica-terraform"
-  location                      = "Central US"
+  name                          = <server-name>
+  resource_group_name           =<resource-name>
+  location                      = <region>
   create_mode                   = "Replica"
-  source_server_id              = "/subscriptions/<subscriptionid>/resourceGroups/gkasar-readreplica-terraform/providers/Microsoft.DBforPostgreSQL/flexibleServers/example"
+  source_server_id              = "/subscriptions/<subscriptionid>/resourceGroups/<resource-name>/providers/Microsoft.DBforPostgreSQL/flexibleServers/<deployed-server-name>"
   version                       = "16"
   public_network_access_enabled = false
   storage_mb                    = 32768
@@ -65,11 +65,11 @@ resource "azurerm_postgresql_flexible_server" "crossregion1" {
 
 }
 resource "azurerm_postgresql_flexible_server" "crossregion2" {
-  name                          = "secondreplica"
-  resource_group_name           ="gkasar-readreplica-terraform"
-  location                      = "Central US"
+  name                          = <server-name>
+  resource_group_name           = <resource-name>
+  location                      = <region>
   create_mode                   = "Replica"
-  source_server_id              = "/subscriptions/<subscriptionid>/resourceGroups/gkasar-readreplica-terraform/providers/Microsoft.DBforPostgreSQL/flexibleServers/example"
+  source_server_id              = "/subscriptions/<subscriptionid>/resourceGroups/<resource-name>/providers/Microsoft.DBforPostgreSQL/flexibleServers/<deployed-server-name>"
   version                       = "16"
   public_network_access_enabled = false
   storage_mb                    = 32768
@@ -81,11 +81,11 @@ resource "azurerm_postgresql_flexible_server" "crossregion2" {
   depends_on=[azurerm_postgresql_flexible_server.crossregion1]
 }
 resource "azurerm_postgresql_flexible_server" "crossregion3" {
-  name                          = "thirdreplica"
-  resource_group_name           ="gkasar-readreplica-terraform"
-  location                      = "Central US"
+  name                          = <server-name>
+  resource_group_name           =<resource-name>
+  location                      = <region>
   create_mode                   = "Replica"
-  source_server_id              = "/subscriptions/<subscriptionid>/resourceGroups/gkasar-readreplica-terraform/providers/Microsoft.DBforPostgreSQL/flexibleServers/example"
+  source_server_id              = "/subscriptions/<subscriptionid>/resourceGroups/<resource-name>/providers/Microsoft.DBforPostgreSQL/flexibleServers/example"
   version                       = "16"
   public_network_access_enabled = false
   storage_mb                    = 32768
@@ -94,5 +94,5 @@ resource "azurerm_postgresql_flexible_server" "crossregion3" {
   sku_name = "GP_Standard_D2ads_v5"
   delegated_subnet_id           = azurerm_subnet.default.id
   private_dns_zone_id           = azurerm_private_dns_zone.default.id
-  depends_on=[azurerm_postgresql_flexible_server.crossregion1]
+  depends_on=[azurerm_postgresql_flexible_server.crossregion2, azurerm_postgresql_flexible_server.crossregion1]
 }
