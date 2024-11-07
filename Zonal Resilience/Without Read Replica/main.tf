f86c2c958b3659446c7034b98ca09b9ddaff1ddc
Gauri Kasar <180354256+gkasar@users.noreply.github.com>
2024-11-06 16:41:24 -0800

    Update with main.tf variable names

--- Zonal Resilience/Without Read Replica/main.tf
+++ Zonal Resilience/Without Read Replica/main.tf
@@ -1,24 +1,29 @@
 provider "azurerm" {
   features {}
+  subscription_id = var.subscriptionID
+}
+resource "random_password" "password" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
 }
 resource "azurerm_resource_group" "default" {
-  name     = <resource-name>
-  location = <region>
+  name     = var.resourceName
+  location = var.location
 }
 resource "azurerm_virtual_network" "example" {
-  name                = <vnet-name>
+  name                = var.virtualNetwork
   location            = azurerm_resource_group.default.location
   resource_group_name = azurerm_resource_group.default.name
   address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_network_security_group" "default" {
-  name                = <nsg-name>
+  name                = var.networkSecurityGroupName
   location            = azurerm_resource_group.default.location
   resource_group_name = azurerm_resource_group.default.name
-
   security_rule {
-    name                       = "test123"
+    name                       = var.sgName
     priority                   = 100
     direction                  = "Inbound"
     access                     = "Allow"
@@ -31,59 +36,40 @@ resource "azurerm_network_security_group" "default" {
 }
 
 resource "azurerm_subnet" "example" {
-  name                 = <subnet-name>
+  name                 = var.subnetName
   resource_group_name  = azurerm_resource_group.default.name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.2.0/24"]
-  service_endpoints    = ["Microsoft.Storage"]
-  delegation {
-    name = "fs"
-    service_delegation {
-      name = "Microsoft.DBforPostgreSQL/flexibleServers"
-      actions = [
-        "Microsoft.Network/virtualNetworks/subnets/join/action",
-      ]
-    }
-  }
-}
 
-
-resource "azurerm_subnet_network_security_group_association" "example" {
-  subnet_id                 = azurerm_subnet.default.id
-  network_security_group_id = azurerm_network_security_group.example.id
-}
-resource "azurerm_private_dns_zone" "example" {
-  name                = "<name>.postgres.database.azure.com"
-  resource_group_name = azurerm_resource_group.default.name
-}
-
-resource "azurerm_private_dns_zone_virtual_network_link" "example" {
-  name                  = "<vnet-name>.com"
-  private_dns_zone_name = azurerm_private_dns_zone.example.name
-  virtual_network_id    = azurerm_virtual_network.example.id
-  resource_group_name   = azurerm_resource_group.default.name
-  depends_on            = [azurerm_subnet.example]
 }
 resource "azurerm_postgresql_flexible_server" "default" {
-  name                          = <server-name>
+  name                          = var.flexibleServeInstance
   resource_group_name           = azurerm_resource_group.default.name
   location                      = azurerm_resource_group.default.location
-  version                       = "16"
-  public_network_access_enabled = false
-  administrator_login           = <user-name>
-  administrator_password        = <password>
-  zone                          = ""
+  version                       = var.pgVersion
+  public_network_access_enabled = true
+  administrator_login = var.username
+  administrator_password = random_password.password.result
   high_availability {
     mode                      = "ZoneRedundant"
   }
 
-  storage_mb                    = 32768
-  storage_tier                  = "P30"
-  sku_name                      = "GP_Standard_D2ads_v5"
-  delegated_subnet_id           = azurerm_subnet.example.id
-  private_dns_zone_id           = azurerm_private_dns_zone.example.id
-  depends_on                    = [azurerm_private_dns_zone_virtual_network_link.example]
+  storage_mb                    = var.storage
+  storage_tier                  = var.storageTier
+  sku_name                      = var.skuName
+
 }
+resource "azurerm_private_endpoint" "example" {
+  name                = var.privateEndpointName
+  location            = var.location
+  resource_group_name = azurerm_resource_group.default.name
+  subnet_id           = azurerm_subnet.example.id
 
+  private_service_connection {
+    name                              = var.privateServiceConnection
+    private_connection_resource_id    = azurerm_postgresql_flexible_server.default.id
+    subresource_names                 = ["postgresqlServer"]
+    is_manual_connection              = false
+  }
 
-
+}
