7a5c7aff65f6927a8eb62c336613887ce30f6149
Gauri Kasar <180354256+gkasar@users.noreply.github.com>
2025-06-11 16:53:58 -0700

    Update main.tf

--- Terraform scripts/Zonal Resilience/With Read Replica/main.tf
+++ Terraform scripts/Zonal Resilience/With Read Replica/main.tf
@@ -43,24 +43,6 @@ resource "azurerm_subnet" "example" {
   address_prefixes     = ["10.0.2.0/24"]
  
 }
-resource "azurerm_subnet_network_security_group_association" "default" {
-  subnet_id                 = azurerm_subnet.example.id
-  network_security_group_id = azurerm_network_security_group.default.id
-}
-resource "azurerm_private_dns_zone" "example" {
-  name                = var.privateDNSZone
-  resource_group_name = var.resourceName
-}
-
-resource "azurerm_private_dns_zone_virtual_network_link" "example" {
-  name                  = var.privateDNSZoneNetworkLink
-  private_dns_zone_name = azurerm_private_dns_zone.example.name
-  virtual_network_id    = azurerm_virtual_network.example.id
-  resource_group_name   = var.resourceName
-  depends_on            = [azurerm_subnet.example]
-}
-
-
 resource "azurerm_postgresql_flexible_server" "default" {
   name                          = var.flexibleServeInstance
   resource_group_name           = var.resourceName
@@ -93,21 +75,6 @@ resource "azurerm_postgresql_flexible_server" "replicaserver1" {
   sku_name                      = var.skuName
   depends_on = [azurerm_postgresql_flexible_server.default]
 }
-resource "azurerm_postgresql_flexible_server" "replicaserver2" {
-  name                = var.readReplica2
-  administrator_login = var.username
-  administrator_password = var.password
-  resource_group_name           = var.resourceName
-  location                      = var.location
-  create_mode                   = "Replica"
-  source_server_id              = azurerm_postgresql_flexible_server.default.id
-  version                       = "16"
-  public_network_access_enabled = false
-  storage_mb                    = var.storage
-  storage_tier                  = var.storageTier
-  sku_name                      = var.skuName
-  depends_on = [azurerm_postgresql_flexible_server.default,azurerm_postgresql_flexible_server.replicaserver1]
-}
 resource "azurerm_private_endpoint" "example" {
   name                = var.privateEndpointName
   location            = var.location
@@ -125,8 +92,8 @@ resource "azurerm_private_endpoint" "example" {
 }
 resource "azurerm_postgresql_flexible_server_virtual_endpoint" "example1" {
   name              = var.virtualendpoint
-  source_server_id  = var.flexibleServeInstance
-  replica_server_id = var.readReplica1
+  source_server_id  = azurerm_postgresql_flexible_server.default.id
+  replica_server_id = azurerm_postgresql_flexible_server.replicaserver1.id
   type              = "ReadWrite"
   depends_on = [azurerm_postgresql_flexible_server.default, azurerm_postgresql_flexible_server.replicaserver1]
 
